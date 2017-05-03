6f5e612d4a202474c7e9fbdcc3d1c4bc36442545
Tandy Sean <tandysean@gmail.com>
2017-05-02 22:24:27 -0400

    initial functioning node app. no separate css file. webpack 1.
    - should update to webpack 2
    - should output a css file

---
+++ reactReviews/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../sass/style.scss';
+
+import App from './components/app';
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>
+  , document.querySelector('.container')
+);
---
+++ reactReviews/webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./",
+    hot: true
+  },
+};
